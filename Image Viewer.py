24f63d5fd03899c3da047d57948597e46f0e5235
krisha232 <78255450+krisha232@users.noreply.github.com>
2022-08-04 18:44:54 +0530

    Add files via upload

---
+++ Image Viewer.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import filedialog
+root = Tk()
+root.title("Image Viewer")
+root.geometry("500x500")
+root.configure(background="black")
+
+
+label_image=Label(root, bg = "white", highlightthickness=5,borderwidth=2, relief=SOLID)
+label_image.place(relx=0.5,rely=0.5,anchor = CENTER)
+
+img_path=""
+
+def OpenFile():
+    global img_path
+    img_path=filedialog.askopenfilename(title="Open Image File", filetypes = [("Image Files","*.jpg;*.gif;*.jpg;;*.png;*txt")])
+    print(img_path)
+    img2_path=Image.open(img_path)
+    img3_path=ImageTk.PhotoImage(img2_path)
+    label_image["image"]=img3_path
+    img3_path.close()
+
+
+openbtn=Button(root, text = "Open File", bg = "white", highlightthickness=5,borderwidth=2, relief=SOLID, command= OpenFile)
+openbtn.place(relx=0.5,rely=0.18,anchor = CENTER)
+
+root.mainloop()
