dd0871e579ec7e17bca4cc4e3e31695cdf3b70de
krisha232 <78255450+krisha232@users.noreply.github.com>
2022-08-10 19:14:24 +0530

    Update Image Viewer.py

--- Image Viewer.py
+++ Image Viewer.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import filedialog
+from tkinter import ttk
 root = Tk()
 root.title("Image Viewer")
 root.geometry("500x500")
@@ -21,8 +22,16 @@ def OpenFile():
     label_image["image"]=img3_path
     img3_path.close()
 
-
+def RotateImg():
+    global img_path
+    img2_path=Image.open(img_path)
+    img3_path=ImageTk.PhotoImage(img2_path.rotate(180))
+    label_image["image"]=img3_path
+    img3_path.close()
 openbtn=Button(root, text = "Open File", bg = "white", highlightthickness=5,borderwidth=2, relief=SOLID, command= OpenFile)
 openbtn.place(relx=0.5,rely=0.18,anchor = CENTER)
+
+rotatebtn=Button(root, text = "Rotate Image", bg = "white", highlightthickness=5,borderwidth=2, relief=SOLID, command= RotateImg)
+rotatebtn.place(relx=0.5,rely=0.88,anchor = CENTER)
 
 root.mainloop()
